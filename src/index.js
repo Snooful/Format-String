3a11c3f4fcb03740c3e3d3487229bd142c5cbf9f
haykam821 <24855774+haykam821@users.noreply.github.com>
2019-03-07 11:16:20 -0500

    Optionally allow first argument to be transformers object

--- src/index.js
+++ src/index.js
@@ -8,6 +8,19 @@ class PlaceholderError extends Error {
 }
 module.exports.PlaceholderError = PlaceholderError;
 
+function trueFormat(transformers = {}, string = "", ...placeholders) {
+	// WIP
+	return false;
+}
+
+/**
+ * Formats a string with optional transformers.
+ */
 function format(...params) {
+	if (typeof params[0] === "string") {
+		return trueFormat({}, ...params);
+	} else {
+		return trueFormat(...params);
+	}
 }
 module.exports.format = format;
