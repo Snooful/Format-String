0b0f891287019843837c7ec5f20a898629c64618
haykam821 <24855774+haykam821@users.noreply.github.com>
2019-03-08 11:29:39 -0500

    Simplify overloading and add support for options object

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ class PlaceholderError extends Error {
 }
 module.exports.PlaceholderError = PlaceholderError;
 
-function trueFormat(transformers = {}, string = "", ...placeholders) {
+function trueFormat(opts = {}) {
 	// WIP
 	return false;
 }
@@ -17,10 +17,34 @@ function trueFormat(transformers = {}, string = "", ...placeholders) {
  * Formats a string with optional transformers.
  */
 function format(...params) {
-	if (typeof params[0] === "string") {
-		return trueFormat({}, ...params);
+	let opts = {
+		transformers: {},
+		template: "",
+		placeholders: [],
+	};
+	
+	// Standardize into an `opts` object
+	if (params.length === 1 && typeof params[0] === "object") {
+		// Singular `opts` parameter
+		opts = Object.assign(opts, params[0]);
+	} else if (typeof params[0] === "object" && typeof params[1] === "string") {
+		// Transformers, template, and rest is placeholders
+		opts.transformers = params[0];
+		opts.template = params[1];
+		opts.placeholders = params.splice(2);
+	} else if (typeof params[0] === "string") {
+		// Template and rest is placeholders
+		opts.template = params[0];
+		opts.placeholders = params.splice(1);
 	} else {
-		return trueFormat(...params);
+		throw new TypeError("Invalid parameters");
 	}
+	
+	// Now check individual types
+	if (typeof opts.transformers !== "object") throw new TypeError("The transformers must be an object.");
+	if (typeof opts.template !== "string") throw new TypeError("The template string must be a string.");
+	if (!Array.isArray(opts.placeholders)) throw new TypeError("The placeholders must be an object.");
+	
+	return trueFormat(opts);
 }
 module.exports.format = format;
